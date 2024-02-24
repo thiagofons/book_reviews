ea60a557c93767f6eb20a330b453a283080ceb49
Thiago Arruda <thiago_arruda@id.uff.br>
2024-02-24 13:52:49 -0300

    feat: get all books

--- final_project/index.js
+++ final_project/index.js
@@ -6,15 +6,42 @@ const genl_routes = require('./router/general.js').general;
 
 const app = express();
 
+const authenticatedUser = (username, password) => {
+  let validusers = users.filter((user) => {
+      return (user.username === username && user.password === password)
+  });
+  if (validusers.length > 0) {
+      return true;
+  } else {
+      return false;
+  }
+}
+
 app.use(express.json());
 
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
-app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+app.use("/customer/auth/*", function auth(req, res, next) {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (!username || !password) {
+      return res.status(404).json({ message: "Error logging in" });
+  }
+  if (authenticatedUser(username, password)) {
+      let accessToken = jwt.sign({
+          data: password
+      }, 'access', { expiresIn: 60 * 60 });
+      req.session.authorization = {
+          accessToken, username
+      }
+      return res.status(200).send("User successfully logged in");
+  } else {
+      return res.status(208).json({ message: "Invalid Login. Check username and password" });
+  }
 });
  
-const PORT =5000;
+const PORT = 5000;
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);

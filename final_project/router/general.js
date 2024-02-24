61d05d66ad815bb202e232317b8a9bb55fd0bf90
Thiago Arruda <thiago_arruda@id.uff.br>
2024-02-24 14:09:44 -0300

    feat: get books by author

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -37,7 +37,29 @@ public_users.get("/isbn/:isbn", function (req, res) {
 // Get book details based on author
 public_users.get("/author/:author", function (req, res) {
   //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const { author } = req.params;
+
+  if (author) {
+    const authorBooks = [];
+
+    for (let book in books) {
+      if (books[book].author.includes(author)) {
+        authorBooks.push(books[book]);
+      }
+    }
+    if (authorBooks.length > 0) {
+      return res.status(200).json({ data: authorBooks });
+    }
+    return res
+      .status(400)
+      .json({
+        data: authorBooks,
+        message: "Author does not exist in the database",
+      });
+  }
+  return res
+    .status(400)
+    .json({ message: "You must provide a valid author name" });
 });
 
 // Get all books based on title

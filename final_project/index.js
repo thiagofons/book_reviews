fae7add721c7fdceb3cf92f8feb6a27b0cc8ea5d
Thiago Arruda <thiago_arruda@id.uff.br>
2024-02-24 15:35:23 -0300

    feat: delete review

--- final_project/index.js
+++ final_project/index.js
@@ -1,48 +1,55 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
-const session = require('express-session')
-const customer_routes = require('./router/auth_users.js').authenticated;
-const genl_routes = require('./router/general.js').general;
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const session = require("express-session");
+const customer_routes = require("./router/auth_users.js").authenticated;
+const genl_routes = require("./router/general.js").general;
+const authenticatedUser = require("./router/auth_users.js").authenticatedUser;
 
 const app = express();
 
-const authenticatedUser = (username, password) => {
-  let validUsers = users.filter((user) => {
-      return (user.username === username && user.password === password)
-  });
-  if (validUsers.length > 0) {
-      return true;
-  } else {
-      return false;
-  }
-}
 
 app.use(express.json());
 
-app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
+app.use(
+  "/customer",
+  session({
+    secret: "fingerprint_customer",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
 
 app.use("/customer/auth/*", function auth(req, res, next) {
-  const {username, password} = req.body;
-  
+  const { username, password } = req.body;
+
   if (!username || !password) {
-      return res.status(404).json({ message: "Error logging in" });
+    return res.status(404).json({ message: "Error logging in" });
   }
   if (authenticatedUser(username, password)) {
-      let accessToken = jwt.sign({
-          data: password
-      }, 'access', { expiresIn: 60 * 60 });
-      req.session.authorization = {
-          accessToken, username
-      }
-      return res.status(200).send("User successfully logged in");
+    let accessToken = jwt.sign(
+      {
+        data: password,
+      },
+      "access",
+      { expiresIn: 60 * 60 }
+    );
+    req.session.authorization = {
+      accessToken,
+      username,
+    };
+    next();
+
+    return res.status(200).send("User successfully logged in");
   } else {
-      return res.status(208).json({ message: "Invalid Login. Check username and password" });
+    return res
+      .status(208)
+      .json({ message: "Invalid Login. Check username and password" });
   }
 });
- 
+
 const PORT = 5000;
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
 
-app.listen(PORT,()=>console.log("Server is running"));
+app.listen(PORT, () => console.log("Server is running"));
--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -17,6 +17,8 @@ const isValid = (username) => {
 };
 
 const authenticatedUser = (username, password) => {
+  console.log(username, password);
+  console.log(users);
   let validusers = users.filter((user) => {
     return user.username === username && user.password === password;
   });
@@ -27,9 +29,7 @@ const authenticatedUser = (username, password) => {
   }
 };
 
-//only registered users can login
 regd_users.post("/login", (req, res) => {
-  console.log(users);
   const username = req.body.username;
   const password = req.body.password;
 
@@ -61,10 +61,38 @@ regd_users.post("/login", (req, res) => {
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const { isbn } = req.params;
+  const { username, review } = req.body;
+
+  if (!review || !username) {
+    return res.status(208).json({ message: "Invalid Review: Check content" });
+  }
+  if (!books[isbn]) {
+    return res.status(208).json({ message: "Invalid ISBN, try again" });
+  } else {
+    books[isbn].reviews[username] = review;
+
+    return res.status(200).json({ data: review });
+  }
+});
+
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+  const { isbn } = req.params;
+  const { username } = req.body;
+
+  if (!review || !username) {
+    return res.status(208).json({ message: "Invalid Review: Check content" });
+  }
+  if (!books[isbn]) {
+    return res.status(208).json({ message: "Invalid ISBN, try again" });
+  } else {
+    delete books[isbn].reviews[username];
+
+    return res.status(200).json({ data: books[isbn].reviews });
+  }
 });
 
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
 module.exports.users = users;
+module.exports.authenticatedUser = authenticatedUser;
